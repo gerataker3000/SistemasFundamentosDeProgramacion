80a11e4101750b8e13191b86701bfbbafd14af20
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-08-29 02:11:03 -0600

    update: Main | only change file to understand github

---
+++ src/Parcial1/IfElse.java
@@ -0,0 +1,12 @@
+package Parcial1;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int edad = 19;
+        if(edad >= 18){
+            System.out.println("GTA6 and resident evil , con el ingeniero oh la ingeniera :D");
+        }else{
+            System.out.println("Canicas, Minecraft, Barbies, Carritos, Tazos ");
+        }
+    }
+}
---
+++ src/Parcial1/IfSencillo.java
@@ -0,0 +1,17 @@
+package Parcial1;
+
+public class IfSencillo {
+    public static void main(String[] args) {
+        int corazones = 48;
+        if(corazones == 13){
+            System.out.println("Completo");
+        }
+
+        if(corazones >= 20){
+            System.out.println("Tienes mods");
+        }
+
+
+        System.out.println("Fin del programa");
+    }
+}
